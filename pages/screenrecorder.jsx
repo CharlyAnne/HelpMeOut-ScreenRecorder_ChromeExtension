9b3ea8abd5efe907b32c9d3e30594011c2267b14
Charly <icharlieanne@gmail.com>
2023-09-30 09:11:32 +0100

    upload

---
+++ pages/screenrecorder.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ExtensionModal from '../components/Extensions/ExtensionModal';
+import RecordModal from '../components/Recorder/RecordModal';
+
+const ScreenRecoder = () => {
+  return (
+    <main className="flex gap-4">
+      <ExtensionModal />
+      <RecordModal />
+    </main>
+  );
+};
+
+export default ScreenRecoder;
