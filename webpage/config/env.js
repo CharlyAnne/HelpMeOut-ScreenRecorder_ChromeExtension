edf7e8c60a980ace1a7a110943ca82ef62964def
Charly <icharlieanne@gmail.com>
2023-10-09 19:29:05 +0100

    delete folder

--- webpage/config/env.js
+++
@@ -1,8 +0,0 @@
-const ENV = {
-  apiUrl:
-    process.env.NODE_ENV === 'development'
-      ? 'http://localhost:3000/api'
-      : 'https://seashell-app-4jicj.ondigitalocean.app/api',
-};
-
-export default ENV;
